9299c9461674c63cefc833f26d7ec585605078cd
Wekoslav Stefanovski <swekster@gmail.com>
2019-09-11 21:26:44 +0200

    Take one of a http server - somewhat successful

--- SedcServer/ServerRunner/Program.cs
+++ SedcServer/ServerRunner/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace ServerRunner
 {
@@ -6,7 +10,41 @@ namespace ServerRunner
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            byte[] bytes = new byte[256];
+            IPAddress localhost = IPAddress.Parse("127.0.0.1");
+
+            TcpListener listener = new TcpListener(localhost, 13000);
+            listener.Start();
+
+
+            Console.WriteLine("Started listening");
+
+            StringBuilder data = new StringBuilder();
+            while(true)
+            {
+                Console.WriteLine("Before client");
+                var client = listener.AcceptTcpClient();
+                Console.WriteLine("After client");
+                var stream = client.GetStream();
+
+                var readCount = stream.Read(bytes, 0, bytes.Length);
+                while (readCount != 0)
+                {
+                    var readString = Encoding.ASCII.GetString(bytes, 0, readCount);
+                    data.Append(readString);
+                    readCount = stream.Read(bytes, 0, bytes.Length);
+                }
+
+                Console.WriteLine(data.ToString());
+
+                var message = " Hello from SEDC";
+                var messageBytes = Encoding.ASCII.GetBytes(message);
+                stream.Write(messageBytes, 0, messageBytes.Length);
+                stream.Flush();
+                stream.Close();
+                client.Close();
+            }
         }
     }
 }
---
+++ SedcServer/StringBuilderDemo/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace StringBuilderDemo
+{
+    class Program
+    {
+        private static List<string> numbers = new List<string> { "one", "two", "three", "four"};
+
+
+        static void Main(string[] args)
+        {
+            var data = Enumerable.Repeat("abcdefg", 80000);
+            Console.WriteLine(data.Count());
+
+            // with string addition
+            Console.WriteLine("--- String Addition ---");
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            var result = string.Empty;
+            foreach (var word in data)
+            {
+                result += word;
+            }
+            stopwatch.Stop();
+            Console.WriteLine(result.Length);
+            Console.WriteLine("Duration: {0} ms",stopwatch.ElapsedMilliseconds);
+
+            Console.WriteLine("--- String Builder ---");
+            stopwatch = Stopwatch.StartNew();
+            var result2 = new StringBuilder();
+            foreach (var word in data)
+            {
+                result2.Append(word);
+            }
+            stopwatch.Stop();
+            var realResult = result2.ToString();
+            Console.WriteLine(realResult.Length);
+            Console.WriteLine("Duration: {0} ms", stopwatch.ElapsedMilliseconds);
+        }
+
+        private static IEnumerable<string> SquareStrings(IEnumerable<string> input)
+        {
+            var result = from a in input
+                         from b in input
+                         select a + b;
+            return result;
+        }
+    }
+}
