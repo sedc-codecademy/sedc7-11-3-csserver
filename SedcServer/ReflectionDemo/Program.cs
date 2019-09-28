6df1d8c9dae28fb7357b708b1adfa5e2e5118e94
Wekoslav Stefanovski <swekster@gmail.com>
2019-09-28 13:26:05 +0200

    Added reflection demo

---
+++ SedcServer/ReflectionDemo/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ReflectionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NormalAccess();
+            ReflectedConstruction();
+            ReflectionFullThrottle();
+        }
+
+        static void NormalAccess()
+        {
+            var person = new Person { FirstName = "Wekoslav", LastName = "Stefanovski" };
+            Console.WriteLine($"{person.LastName}, {person.FirstName}");
+            Console.WriteLine(person);
+        }
+
+        static void ReflectedConstruction()
+        {
+            var person = (Person)Activator.CreateInstance(typeof(Person));
+            person.FirstName = "Wekoslav";
+            person.LastName = "Stefanovski";
+            Console.WriteLine($"{person.LastName}, {person.FirstName}");
+            Console.WriteLine(person);
+        }
+
+        static void ReflectionFullThrottle()
+        {
+            var person = Activator.CreateInstance(typeof(Person));
+            typeof(Person).GetProperty("FirstName").GetSetMethod().Invoke(person, new object[] { "Wekoslav" });
+            typeof(Person).GetProperty("LastName").GetSetMethod().Invoke(person, new object[] { "Stefanovski" });
+            var name = typeof(Person).GetProperty("FirstName").GetGetMethod().Invoke(person, null);
+            var surname = typeof(Person).GetProperty("LastName").GetGetMethod().Invoke(person, null);
+            Console.WriteLine($"{surname}, {name}");
+            Console.WriteLine(person);
+        }
+
+    }
+
+
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}";
+        }
+    }
+}
